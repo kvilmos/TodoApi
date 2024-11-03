e2bc94ac363ac503dc36330391ab2009aed1e6f7
kvilmos <kvilmos47@gmail.com>
2024-11-03 09:03:20 +0100

    Web API with JavaScript

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -28,6 +28,9 @@ namespace TodoApi
                 app.UseSwaggerUI();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
